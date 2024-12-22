78e119109e18c88e94f14a3da377ca28e64db8d8
Muhammad-Shah-zaib <MuhammdShahzaib@outlook.com>
2024-12-22 19:32:16 +0500

    added Program.cs again

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿global using SmartRide.Models;
+global using SmartRide.src.Dtos;
+global using SmartRide.src.DataStructures;
+global using SmartRide.src.Services;
+global using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Pika Pika");
